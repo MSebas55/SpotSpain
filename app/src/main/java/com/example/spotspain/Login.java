9e4e1d9904c1a60cdf24bda8d07b704bbd11239a
MSebas55 <calimandrucsebas@gmail.com>
2023-11-07 20:33:24 +0100

    Login, Register y SplashScreen
    Falta insertar base de datos

--- app/src/main/java/com/example/spotspain/Login.java
+++ app/src/main/java/com/example/spotspain/Login.java
@@ -1,7 +1,10 @@
 package com.example.spotspain;
 
         import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void changeToRegister(View view) {
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
     }
 }
