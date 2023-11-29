9bb42445ba4793e353701eece221a9c2136c56b7
Alberto <alesdo2003@gmail.com>
2023-11-29 01:12:43 +0100

    Ahora si que esta ready. Creo...

---
+++ app/src/main/java/com/example/spotspain/SearchActivity.java
@@ -0,0 +1,37 @@
+package com.example.spotspain;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.spotspain.databinding.ActivitySearchBinding;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private ActivitySearchBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String[] provincias = getResources().getStringArray(R.array.Provincia);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, R.layout.list_item, provincias);
+
+        String[] municipios = getResources().getStringArray(R.array.Municipio);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, R.layout.list_item, municipios);
+
+        String[] intereses = getResources().getStringArray(R.array.Interes);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, R.layout.list_item, intereses);
+
+        AutoCompleteTextView autoCompleteTextView1 = binding.autoCompleteTextView1;
+        autoCompleteTextView1.setAdapter(adapter1);
+
+        AutoCompleteTextView autoCompleteTextView2 = binding.autoCompleteTextView2;
+        autoCompleteTextView2.setAdapter(adapter2);
+
+        AutoCompleteTextView autoCompleteTextView3 = binding.autoCompleteTextView3;
+        autoCompleteTextView3.setAdapter(adapter3);
+    }
+}
